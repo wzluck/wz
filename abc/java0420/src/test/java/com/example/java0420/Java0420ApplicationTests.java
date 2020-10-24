7ae191106ab44d19d4e33d35a2f436b7480a8f3c
wangzhi <593263391@qq.com>
2020-10-24 14:55:36 +0800

    aaaaaa

---
+++ abc/java0420/src/test/java/com/example/java0420/Java0420ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java0420;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java0420ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
